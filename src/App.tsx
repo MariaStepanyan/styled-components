755aa80ab72d57a922784a8c14edb491fb21d2d4
MariaStepanyan <drmariastepanyan@gmail.com>
2023-12-19 15:01:54 +0300

    refacter media

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
-import React from 'react'
 import styled from 'styled-components'
 import './App.css'
 import { StyledBtn } from './components/button.styled'
 import { Link } from './components/Link.styled'
-import { Menu } from './components/Menu.styled'
 import { myTheme } from './styles/Theme.styled'
 
 function App() {
   return (
     <div className="App">
-      {/* <Menu>
-        <ul>
-          <li>
-            <a href="">menu item 1</a>
-          </li>
-          <li>
-            <a href="">menu item 2</a>
-          </li>
-          <li>
-            <a href="">menu item 3</a>
-          </li>
-        </ul>
-      </Menu> */}
       <Box>
-        {/* <StyledBtn as={Link} href="#">
-          Link component
-        </StyledBtn>
-        <StyledBtn as="a" href="#">
-          Link
-        </StyledBtn> */}
-        {/* <StyledBtn color="blue" fontSize ="20px">Hello</StyledBtn>
-        <StyledBtn color="brown">Hello</StyledBtn> */}
         <StyledBtn $btnType={'primary'} color={myTheme.colors.primary} $active>
           Hello
         </StyledBtn>
--- src/styles/Theme.styled.tsx
+++ src/styles/Theme.styled.tsx
@@ -9,7 +9,7 @@ export const myTheme = {
   },
 
   media: {
-    tablet: 'screen and(max-width: 768px)',
-    mobile: 'screen and(max-width: 576px)',
+    tablet: 'screen and (max-width: 768px)',
+    mobile: 'screen and (max-width: 576px)',
   },
 }
